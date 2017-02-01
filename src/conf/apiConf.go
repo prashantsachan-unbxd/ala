36e36b444a6c6059a2abba7a299739fd6a9f00d6
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-02-01 09:57:04 +0530

    feat(conf):Add tags to apiConfiguration model

--- src/conf/apiConf.go
+++ src/conf/apiConf.go
@@ -7,10 +7,12 @@ import(
 type basicConf struct{
     Api api.Api `json:"api"`
     ValidatorType string `json:"validator_type"`
+    Tags []string `json:"tags"`
 }
 type ApiConf struct{
     Api api.Api 
     Validator api.ApiValidator
+    Tags []string `json:"tags"`
 }
 type ConfLoader interface{
     ReadApiConf() ([]ApiConf,error)
--- src/conf/util.go
+++ src/conf/util.go
@@ -5,9 +5,9 @@ import(
 
 func fromBasic(src basicConf)ApiConf{
    valid:= GetValidator(src.ValidatorType)
-   return ApiConf{src.Api, valid}    
+   return ApiConf{src.Api, valid, src.Tags}    
 }
 func toBasic(src ApiConf)basicConf{
     validType:= GetValidatorType(src.Validator)
-    return basicConf{src.Api, validType}
+    return basicConf{src.Api, validType, src.Tags}
 }
