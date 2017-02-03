9f278e4aa3ed6c7b75a2ef6f79c811e11e342308
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-02-03 15:11:24 +0530

    ui(feat)add tag filtering to GET apiConfig API

--- src/ui/confController.go
+++ src/ui/confController.go
@@ -12,15 +12,36 @@ type ConfController struct{
 }
 
 func (h *ConfController) Register(r *mux.Router){
-    r.Methods("GET").Path("/conf/api").HandlerFunc(h.getAllApis)
+    r.Methods("GET").Path("/conf/api").HandlerFunc(h.getConfs)
 }   
-
-func (h *ConfController)getAllApis(w http.ResponseWriter, r *http.Request){
-    js,err:= json.Marshal(h.Configs)
-    if err!=nil{
+func (h *ConfController)getConfs(w http.ResponseWriter, r *http.Request){
+    tags, ok:= r.URL.Query()["tags"]
+    filtered  := *h.Configs
+    if ok && len(filtered)>0{
+       filtered = filter(filtered, tags)
+    }
+    js,err:= json.Marshal(filtered)
+    if err != nil{
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
     w.Header().Set("Content-Type", "application/json")
     w.Write(js)
+}
+func filter(confs []conf.ApiConf, tags []string)[]conf.ApiConf{
+    var filtered [] conf.ApiConf
+    for _, c := range confs{
+        found := false
+        for _, t := range c.Tags{
+            if(found) {break}
+            for _,rt := range tags{
+                if(t == rt){
+                    found = true
+                    filtered  = append(filtered, c)
+                    break
+                }
+            }
+        }
+    }
+    return filtered
 }
