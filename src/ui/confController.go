9897222eea6d002541c756a6bac82b1db261bf1f
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-02-05 15:16:28 +0530

    feat(conf)Add api to add new ApiConf to the tracking list

--- src/ui/confController.go
+++ src/ui/confController.go
@@ -4,6 +4,7 @@ import(
     "net/http"
     mux "github.com/gorilla/mux"
     "encoding/json"
+    "io/ioutil"
      )
 
 type ConfController struct{
@@ -13,7 +14,28 @@ type ConfController struct{
 
 func (h *ConfController) Register(r *mux.Router){
     r.Methods("GET").Path("/conf/api").HandlerFunc(h.getConfs)
+    r.Methods("PUT").Path("/conf/api").HandlerFunc(h.addConf)
 }   
+func (h *ConfController)addConf(w http.ResponseWriter, r *http.Request){
+    data, err := ioutil.ReadAll(r.Body)
+    if err!= nil{
+        http.Error(w, "unable to read data:\n"+err.Error(), http.StatusBadRequest)
+        return
+    }
+    newConf,err := conf.FromJson(string(data))
+    if err != nil{
+        http.Error(w, "unable to create apiConf from data\n"+ err.Error(), http.StatusBadRequest)
+        return
+    }
+    newConfigSet := append(*h.Configs, newConf)
+    err= h.ConfWriter.WriteApiConf(newConfigSet)
+    if err!=nil{
+        http.Error(w,"unable to persist new apiConf\n"+ err.Error(), http.StatusInternalServerError)
+        return
+    }
+    *h.Configs = newConfigSet
+    w.Write([]byte("successfully added new api to tracking list"))
+}
 func (h *ConfController)getConfs(w http.ResponseWriter, r *http.Request){
     tags, ok:= r.URL.Query()["tags"]
     filtered  := *h.Configs
