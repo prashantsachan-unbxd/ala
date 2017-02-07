c7781a3f445702067b0fd34296858c536200ce01
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-02-07 13:19:01 +0530

    feat(ui):Add Delte API for apiConfigurations

--- src/ui/confController.go
+++ src/ui/confController.go
@@ -5,6 +5,8 @@ import(
     mux "github.com/gorilla/mux"
     "encoding/json"
     "io/ioutil"
+    "github.com/satori/go.uuid"
+    "errors"
      )
 
 type ConfController struct{
@@ -15,7 +17,42 @@ type ConfController struct{
 func (h *ConfController) Register(r *mux.Router){
     r.Methods("GET").Path("/conf/api").HandlerFunc(h.getConfs)
     r.Methods("PUT").Path("/conf/api").HandlerFunc(h.addConf)
+    r.Methods("DELETE").Path("/conf/api/{confId}").HandlerFunc(h.deleteConf)
 }   
+func (h *ConfController)deleteConf(w http.ResponseWriter, r* http.Request){
+    pVars:= mux.Vars(r)
+    confId,ok := pVars["confId"] 
+    if !ok{
+        http.Error(w, "apiConf Id passed is empty", http.StatusBadRequest)
+        return
+    }else if err:=h.delConf(confId);err !=nil{ 
+        http.Error(w, "unable to delete apiConf with id: "+ confId+"\n"+
+            err.Error(), http.StatusInternalServerError)
+        return
+    }else{
+        w.Write([]byte("deleted the apiConf with Id: "+confId))
+        return
+    }
+}
+func (h *ConfController)delConf(confId string) error{
+    id,err:= uuid.FromString(confId)
+    if err !=nil{
+        return err
+    }
+
+    for idx,cnf := range *h.Configs{
+        if cnf.Id == id{
+            *h.Configs = append((*h.Configs)[:idx], (*h.Configs)[idx+1:]...) 
+            err:= h.ConfWriter.WriteApiConf(*h.Configs)
+            if err!=nil{
+                return err
+            }else{
+                return nil
+            }
+        }
+    }
+    return errors.New("No apiConf found with ID: "+id.String())
+}
 func (h *ConfController)addConf(w http.ResponseWriter, r *http.Request){
     data, err := ioutil.ReadAll(r.Body)
     if err!= nil{
