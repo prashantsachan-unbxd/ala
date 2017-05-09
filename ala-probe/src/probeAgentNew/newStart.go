f587c84f8bfc962c015f20763f6bd375a72e050a
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-05-09 19:07:40 +0530

    feat(probe):Connect EventConsumer to Executor

--- ala-probe/src/probeAgentNew/newStart.go
+++ ala-probe/src/probeAgentNew/newStart.go
@@ -3,11 +3,9 @@ package main
 import(
     "fmt"
     "time"
-    // mux "github.com/gorilla/mux"
-    // "net/http"
     topo "topology"
     "execute"
-    "encoding/json"
+    "execute/result"
 
 )
 
@@ -16,20 +14,16 @@ func main(){
     serviceDao.Init()
     var REDao execute.RuleEngineDao = execute.RuleEngineDao{"http://ec2-54-173-96-124.compute-1.amazonaws.com", 8081, "",""}
     var exec execute.Executor
-//    exec = & ex.SingleExec{cnfMgr}
     exec  = & execute.IntervalExec{Interval:7* time.Second, ServiceStore:serviceDao, REDao: REDao}
     fmt.Println("starting executor")
-    out:= exec.StartExec()
-    for{
-        v:= <- out
-        eStr,jErr:= json.Marshal(v)
-        if jErr !=nil{
-            fmt.Println("error in marshalling evnt", jErr)
-        }else{
-            fmt.Println("event Received:", string(eStr))
-        }
-    }
     
+    
+    out:= exec.StartExec()
+    dispatcher := result.SimpleDispatcher{Consumers:[]result.EventConsumer{&result.EventLogger{}, & result.KafkaForwarder{[]string{"localhost:9092"}}}}
+    // dispatcher := result.SimpleDispatcher{Consumers:[]result.EventConsumer{&result.EventLogger{}}}
+    dispatcher.StartDispatch(out)
+    //wait forever
+    select{}
     
     // exec.StopExec()
     
