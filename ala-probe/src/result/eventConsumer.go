36b8322ed93cc36835a0135bc5a324db8d574be9
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-05-09 19:16:14 +0530

    refactor(probe):Remove version 0.1 code

--- ala-probe/src/result/eventConsumer.go
+++
@@ -1,9 +0,0 @@
-package result
-
-import(
-    "ex"
-    )
-
-type EventConsumer interface{
-    Consume(e ex.Event)
-}
--- ala-probe/src/ui/reqController.go
+++
@@ -1,7 +0,0 @@
-package ui
-import (
-    mux "github.com/gorilla/mux"
-    )
-type ReqController interface {
-    Register(r *mux.Router)
-}
