d43132689a7292330b69498a1eb4d660d1c6f76a
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-05-24 17:55:40 +0530

    fix(probe/conf):make config keys constant

--- ala-probe/src/probeAgent/consts.go
+++ ala-probe/src/probeAgent/consts.go
@@ -4,22 +4,22 @@ import(
 	"github.com/spf13/viper"
 )
 
-var conf_batch_interval = "execute.batch_interval"
-var conf_log_level = "log_level"
+const conf_batch_interval = "execute.batch_interval"
+const conf_log_level = "log_level"
 
 //execute section
-var secEx = "execute."
-var conf_service_path =  secEx+"path_service_conf"
+const secEx = "execute."
+const conf_service_path =  secEx+"path_service_conf"
 // Rule Engine section
-var secRE = "ruleEngine."
-var conf_re_host = secRE + "host"
-var conf_re_port = secRE + "port"
-var conf_re_user = secRE + "user"
-var conf_re_pass = secRE + "pass"
+const secRE = "ruleEngine."
+const conf_re_host = secRE + "host"
+const conf_re_port = secRE + "port"
+const conf_re_user = secRE + "user"
+const conf_re_pass = secRE + "pass"
 
 // Kafka section
-var secKafka = "kafka."
-var conf_kafka_brokers = secKafka+ "brokers_hostport"
+const secKafka = "kafka."
+const conf_kafka_brokers = secKafka+ "brokers_hostport"
 
 var mustConfs = []string{conf_batch_interval, 
 	conf_service_path,
