6df5d2da0b4e3f3e456130b68906219ef188c3a1
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-05-24 14:48:48 +0530

    feat(probe):Read Application Configuration from file
    * Read configs from Toml format file
    * Check existance of mandatory keys in conf
    * Accept config File as cli argument
    running application with command
      - `bin/ProbeAgent` will use the default config file
      - `bin/ProbeAgent someFilePath` will find a file `someFilePath.toml` & use it
    by default `someFilePath.toml` is search in
      - current directory(".")
      - user's home directory ( $HOME)
      - root directory ("/")
    remember that extention `.toml` should not be passed
    
    closes #9

--- ala-probe/src/probeAgent/start.go
+++ ala-probe/src/probeAgent/start.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+    "fmt"
+    "github.com/spf13/viper"
     log "github.com/Sirupsen/logrus"
     "time"
     "os"
@@ -9,21 +11,72 @@ import(
     "result"
 
 )
+var confFile = "resource/appConf"
 func init(){
+    cliArgs := os.Args[1:]
+    if( len(cliArgs) !=1){
+        fmt.Println("continuing with default conf file", confFile)
+    }else{
+        confFile = cliArgs[0]    
+    }
+    
+    viper.SetConfigName(confFile)     // no need to include file extension
+    viper.AddConfigPath("/")
+    viper.AddConfigPath(".")
+    viper.AddConfigPath("$HOME")
+    // viper.AddConfigPath("../../resource/")  // set the path of your config file
+    err := viper.ReadInConfig()
+    if err != nil {
+        fmt.Println("Failed to load configs from", confFile)
+        fmt.Println(err)
+    }else{
+        fmt.Println("Configs loaded from file:", confFile)
+    }
     log.SetFormatter(&log.JSONFormatter{})
     log.SetOutput(os.Stdout)
-    log.SetLevel(log.DebugLevel)
+    logLevel:= viper.GetString(conf_log_level)
+    lvl,err := log.ParseLevel(logLevel)
+    if !viper.IsSet(conf_log_level){
+        log.SetLevel(log.DebugLevel)
+        log.WithFields(log.Fields{"module":"main",
+         "error":"missing conf log_level"}).Info("setting log level to default: Debug")
+    }else if err !=nil{
+        log.SetLevel(log.DebugLevel)
+        log.WithFields(log.Fields{"module":"main", "error":err}).Info("error parsinglog level,setting to default: Debug")
+    }else{
+        log.SetLevel(lvl)
+        log.WithFields(log.Fields{"module":"main"}).Info("setting log level to:"+logLevel)
+    }
 }
 func main(){
-    var serviceDao topo.ServiceDao = &topo.FileServiceDao{"./resource/serviceConf.json"}
+    m:= missingConfs();
+    if len(m)>0{
+        log.WithFields(log.Fields{"module":"main","confFields":m,"error":"missing mandatory confs"}).Panic("shutting down")
+
+    }
+    var serviceDao topo.ServiceDao = &topo.FileServiceDao{viper.GetString(conf_service_path)}
     serviceDao.Init()
-    var REDao execute.RuleEngineDao = execute.RuleEngineDao{"http://ec2-54-173-96-124.compute-1.amazonaws.com", 8081, "",""}
+    var REDao execute.RuleEngineDao = execute.RuleEngineDao{viper.GetString(conf_re_host),
+     viper.GetInt(conf_re_port), viper.GetString(conf_re_user),viper.GetString(conf_re_pass)}
     var exec execute.Executor
-    exec  = & execute.IntervalExec{Interval:7* time.Second, ServiceStore:serviceDao, REDao: REDao}
+    var batchInterval = 7*time.Second
+    if(!viper.IsSet(conf_batch_interval)){
+        log.WithFields(log.Fields{"module":"main", "error":"batch_interval conf missing"}).Error("setting default: "+batchInterval.String())
+    }else{
+        batchInterval,err := time.ParseDuration(viper.GetString(conf_batch_interval))
+        if err !=nil{
+            log.WithFields(log.Fields{"module":"main","error":err}).Error("unable to parse batch-interval from conf")
+        }else{
+            log.WithFields(log.Fields{"module":"main"}).Info("setting batch interval to "+batchInterval.String())    
+        }
+        
+    }
+    exec  = & execute.IntervalExec{Interval:batchInterval, ServiceStore:serviceDao, REDao: REDao}
     log.WithFields(log.Fields{"module": "main",}).Info("starting executor")    
     
     out:= exec.StartExec()
-    dispatcher := result.SimpleDispatcher{Consumers:[]result.EventConsumer{&result.EventLogger{}, & result.KafkaForwarder{[]string{"localhost:9092"}}}}
+    dispatcher := result.SimpleDispatcher{Consumers:[]result.EventConsumer{&result.EventLogger{}, 
+        & result.KafkaForwarder{viper.GetStringSlice(conf_kafka_brokers)}}}
     // dispatcher := result.SimpleDispatcher{Consumers:[]result.EventConsumer{&result.EventLogger{}}}
     log.WithFields(log.Fields{"module": "main",}).Info("starting dispatcher")
     dispatcher.StartDispatch(out)
