4e50721f60dd20ee4727298e9d74d2d79528cb4a
prashantsachan-unbxd <prashant.sachan@unbxd.com>
2017-05-30 17:35:32 +0530

    fix(probe/execute):Multiple probeRequest in parallely
    earlier:
        In case of multiple probeConfigs, All the probeRequests sent were same
    (there was only one probeConfig was being used multiple times)
    reason:
         Golang closure takes reference of the variables defined earlier. In
    case of a loop, starting a new go-routine on each iteration, it is possible that
    by the time a child go-routine accesses the variable, its value may have been changed
    because of further loop iterations. This caused (most of the times) the last value of
    variable is used in every go-routine, which is the last ProbeConfig in our case
    
    now:
       pass the changing variable to the child go-routine as a function param. This way,
    the values passed will not change due to further loop iteration
    
    closes #39

--- ala-probe/src/client/ProbeClient.go
+++ ala-probe/src/client/ProbeClient.go
@@ -8,10 +8,14 @@ import (
 //ProbeClient implementation contains the logic to connect and
 // retrieve information from a service. 
 //examples of ProbeClient implementation could be HTTP, mongo, zookeeper etc.
+//      Also, each instance of a client should be independent from other so that 
+// running them in parallel doesn't have any side effect (no write-able global variables)
 type ProbeClient interface{
     //isEmpty checks whether the instance is empty or not (equivalent to null check)
     isEmpty()bool
     // New returns a new Instance of this type, initialized with the supplied config
+    // This method should be very light in general, as the executor will be creating 
+    // new client instance for every probe request.
     New(config map[string]interface{}, service topo.Service) (ProbeClient,error)
     // Execute runs the probe request as per config & return a ProbeResponse
     Execute()(resp.ProbeResponse, error)
